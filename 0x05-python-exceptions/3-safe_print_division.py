4040821a4b18a3cdd4b30b3687b14b8f418bf9da
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-06-26 15:14:36 +0300

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = a / b
+        value = a / b
     except ZeroDivisionError:
-        print("Inside result: None")
-        print("Error: Division by zero is not allowed.")
-        return None
-    except Exception as e:
-        print("Inside result: None")
-        print("Error:", str(e))
-        return None
-    else:
-        print("Inside result: {}".format(result))
-        return result
+        value = None
     finally:
-        print("Finally block executed.")
+        print('Inside result: {}'.format(value))
+        return value
