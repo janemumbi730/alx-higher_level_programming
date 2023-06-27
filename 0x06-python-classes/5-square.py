8bea3ad3189a3ace909a67c8cee880f5e1fb249e
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-06-27 14:21:54 +0300

    correction for 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,36 +1,47 @@
 #!/usr/bin/python3
-"""Module 5-square"""
+"""
+Create a Class Square with:
+- size proprety
+- method of area and method of print_square
+- getters & setters.
+"""
 
 
 class Square:
-    """created class"""
+    """Class - Square"""
+
     def __init__(self, size=0):
-        """private instance attribute - size"""
-        if type(size)is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+        """Constructor of a Square with the size"""
+        if (type(size) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (size < 0):
+            raise (ValueError("size must be >= 0"))
         else:
             self.__size = size
 
+    def area(self):
+        """Method to get the area of the Square"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """Method to print a Square"""
+        if (self.__size == 0):
+            print()
+        else:
+            for rows in range(self.__size):
+                print("#" * self.__size)
+
     @property
     def size(self):
-        """"getter"""
-        return self.__size
+        """Getter of the private attribute size"""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """Setter"""
-
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
+        """Setter for the size private attribute"""
+        if (type(value) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (value < 0):
+            raise (ValueError("size must be >= 0"))
         else:
             self.__size = value
-
-    def area(self):
-        return (self.__size)**2
-
-    def my_print(self):
-        print("\n".join(["#" * self.__size for rows in range(self.__size)]))
