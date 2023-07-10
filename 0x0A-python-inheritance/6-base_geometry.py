3cc478b74dd594d3e748c0ccf9110941edc13558
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-07-10 19:35:24 +0300

    Update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-class BaseGeometry():
+"""
+Module 6-base_geometry
+Has a public instance method
+Raises an exception
+"""
+
+
+class BaseGeometry:
+    """With public instance method
+    methods:
+    area(self)
+    """
     def area(self):
-        raise Exception('area() is not implemented')
+        raise Exception("area() is not implemented")
