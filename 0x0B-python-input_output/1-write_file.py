000cd6e2d7a1ad28a5a2ea778bc54df1017efc0a
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-07-11 14:15:01 +0300

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 """
-Module 3-write_file
-Contains function that writes to text file and returns num chars written
+This program writes in a file if doesn't exists create the file
 """
 
 
 def write_file(filename="", text=""):
-    """writes to text file and returns num chars written"""
-    with open(filename, mode="w", encoding="utf-8") as f:
-        return(f.write(text))
+    """
+    Write in a file, if doesn't exists create the file
+    Args:
+      - filename: string
+      - text: string
+    """
+
+    with open(filename, mode="w", encoding="utf-8") as _file:
+        _file.write(text)
+
+    return (len(text))
