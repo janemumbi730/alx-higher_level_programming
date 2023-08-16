9dea12923f0c118fc4e0349a3193f4fd147d93fc
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-16 13:00:29 +0300

    Update 13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,2 @@
 -- lists all genres and numbers of shows linked to it
-SELECT tv_genres.name, COUNT(*) AS number_of_shows
-FROM tv_genres
-GROUP BY tv_shows.name
-INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id;
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_genres JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
