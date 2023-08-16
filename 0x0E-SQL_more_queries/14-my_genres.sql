b5cf512b82f1067e513907c4433ccc2ec3ad1779
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-16 13:01:40 +0300

    Update 14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,9 @@
 -- shows all genre of the show dexter
-SELECT tv_genres.name AS name
+SELECT tv_genres.name
 FROM tv_genres
-LEFT JOIN tv_show_genres
-	ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows
-	ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows.title = 'Dexter';
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name;
