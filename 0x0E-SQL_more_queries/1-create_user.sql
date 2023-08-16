0f8c437d0b887d835d23cbbcff30084a1be6c8e1
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-16 13:05:26 +0300

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
--- creates user user_0d_1 and grants all privileges
+-- creates user user_0d_1
+-- user will have all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
