127c1411603cfbfd8fe3563a389d64836d0ac52e
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-16 12:58:44 +0300

    Update 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,4 @@
--- creates database hbtn_0d_usa
--- creates table states
+-- creates database hbtn_0d_usa and table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE 'hbtn_0d_usa';
-CREATE TABLE IF NOT EXISTS states (
-	id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-	name VARCHAR(256) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(256) NOT NULL);
