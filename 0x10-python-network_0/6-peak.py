32c05ff3df918ad2480c044bf69f82c719d9278d
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-09-29 04:48:42 +0300

    Create 6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""module 6 peak"""
+
+
+def find_peak(list_of_integers):
+    """
+    return peak in a list of unsorted integers
+    """
+    if not list_of_integers:
+        return None
+
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+
+    return list_of_integers[left]
