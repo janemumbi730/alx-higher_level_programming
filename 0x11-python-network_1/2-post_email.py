bbda14dedb2e0e90778be83db4be0220864caa86
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-09-30 00:13:10 +0300

    Create 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Sends post request with parameters"""
+
+import urllib.request
+from sys import argv
+import urllib.parse
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = {'email': argv[2]}
+
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode())
