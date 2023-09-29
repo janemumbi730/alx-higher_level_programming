48808cf09ca05e314fb4d4482fb8a2a5e31caa28
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-09-30 00:15:37 +0300

    Create 3-error_code.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Manage exceptions"""
+
+if __name__ == '__main__':
+    import sys
+    from urllib import request, error
+
+    argv = sys.argv
+    url = argv[1]
+    try:
+        with request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as err:
+        print("Error code: {}".format(err.status))
