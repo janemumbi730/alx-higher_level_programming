47e7e9a58b814dadeb143b805c81894fd4496ca0
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-09-30 00:25:04 +0300

    Create 10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Get github id"""
+
+from sys import argv
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    r = requests.get(url, auth=HTTPBasicAuth(argv[1], argv[2]))
+    print(r.json().get('id'))
